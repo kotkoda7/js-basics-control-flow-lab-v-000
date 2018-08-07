d361b479d89ce9ed7bd44dac7d2f2a7b379dbf99
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-08-07 14:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function scuberGreetingForFeet (feet) {
  else if  (feet > 199 && feet < 2000) {
   return "I will gladly take your thirty bucks.";
   
-} else (feet > 2500) {
+} else { (feet > 2500) {
   return 'No can do.';
 }
 }
