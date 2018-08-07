52dca4345d8c183ca0c0b20dbb32053f30fdf85b
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-08-07 14:34:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,5 +25,9 @@ function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous':
       return "Thank you so much.";
+    case 'not as generous' :
+      return 'Thank you.'
+    default:
+     return 'Bye'
   }
 }
