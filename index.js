6bc45b0b5eb21b534c5554222b6a91462f61fd90
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-08-07 14:11:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-function scuberGreetingForFeet (feet);
+function scuberGreetingForFeet (feet)
 if (scuberGreetingForFeet <= 199) {
   return 'This one is on me!';
 }
